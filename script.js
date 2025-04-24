b7839fd8f782d7008a6a7ba14ebb95e5417e09fd
Jishadh <muhmmedjishad857@gmail.com>
2025-04-24 22:25:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function downloadPDF() {
     const cloned = element.cloneNode(true);
     cloned.style.backgroundColor = "#252525";
     cloned.style.color = "white";
-    cloned.style.padding = "20px";
+    cloned.style.padding = "20px 20px 40px 20px"; // top, right, bottom, left
     cloned.style.borderRadius = "10px";
     cloned.style.width = "100%";
     cloned.style.boxSizing = "border-box";
@@ -22,7 +22,7 @@ function downloadPDF() {
     // Container to wrap the styled resume
     const container = document.createElement('div');
     container.style.backgroundColor = "#252525";
-    container.style.padding = "20px";
+    container.style.padding = "20px 20px 40px 20px"; // top, right, bottom, left
     container.style.color = "white";
     container.style.width = "100%";
     container.style.fontFamily = "Arial, sans-serif";
